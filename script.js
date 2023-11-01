ae570739fffb5948e1d88a197d82788e5defaff9
Allexxxus <72846177+Allexxxus@users.noreply.github.com>
2023-11-01 09:54:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const socket = new WebSocket('ws://localhost:3000/ws');
+
+console.log('Сайт работает');
+
+let clickme = document.querySelector('#clickme');
+let dynamicCounter = document.querySelector('#dynamicCounter');
+let clickCount = 0;
+
+socket.addEventListener('open', (event) => {
+  console.log('открыто WebSocket соединение:', event);
+});
+
+socket.addEventListener('message', (event) => {
+  console.log('Сообщение с бекенда:', event.data);
+  clickCount = parseInt(event.data); 
+  dynamicCounter.textContent = `${clickCount}`;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  clickme.addEventListener('click', function() {
+    socket.send('Clicked!');
+  });
+});
